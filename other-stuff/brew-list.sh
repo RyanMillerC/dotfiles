19fe9eb59834d939a6f22b77ae95ee36556c120e
Ryan Miller <RyanMillerC@outlook.com>
2025-10-02 13:14:30 -0400

    Use 'full-name' for brew list

--- other-stuff/brew-list.sh
+++ other-stuff/brew-list.sh
@@ -2,10 +2,10 @@
 
 # Get only things installed from "brew install X"
 echo '# brew installed packages'
-brew list --installed-on-request -1
+brew list --full-name --installed-on-request -1
 
 echo '' # Newline
 
 # Casks aren't included in the above list, so print all casks
 echo '# brew installed casks'
-brew list --casks -1
+brew list --full-name --casks -1
